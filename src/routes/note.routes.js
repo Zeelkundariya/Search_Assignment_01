4220bcdebc64850a67dd230546585fc42c418404
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-29 12:56:45 +0530

    feat: add bulk create note endpoint (POST /api/notes/bulk)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { createNote } = require("../controllers/note.controller");
+const { createNote, createBulkNotes } = require("../controllers/note.controller");
+
+// CRUD bulk
+router.post("/bulk", createBulkNotes);
 
 // CRUD single-item routes
 router.post("/", createNote);
