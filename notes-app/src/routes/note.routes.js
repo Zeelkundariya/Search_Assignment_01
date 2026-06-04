5d7565f97f60b73c9909f805de68d973ab53caed
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-04 16:53:01 +0530

    feat: add get all notes endpoint (GET /api/notes)

--- notes-app/src/routes/note.routes.js
+++ notes-app/src/routes/note.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes } = require("../controllers/note.controller");
 
 // CRUD bulk
 router.post("/bulk", createBulkNotes);
 
 // CRUD single-item routes LAST
 router.post("/", createNote);
+router.get("/", getAllNotes);
 
 module.exports = router;
